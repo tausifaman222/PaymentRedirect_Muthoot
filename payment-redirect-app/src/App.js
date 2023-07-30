b51a87008dc835ea721e3c78c354b183778f6ea4
T@uSiF <cse1887@cemk.ac.in>
2023-07-30 17:02:41 +0530

    Initial Page

---
+++ payment-redirect-app/src/App.js
@@ -0,0 +1,57 @@
+import React, { useState } from 'react';
+import './App.css';
+
+function App() {
+  const [paymentLink, setPaymentLink] = useState('');
+  const [showWelcomeMessage, setShowWelcomeMessage] = useState(true);
+
+  const redirectToPayment = () => {
+    if (paymentLink.trim() !== '') {
+      // Check if the link includes "https://" and "www"
+      if (!paymentLink.includes('https://www')) {
+        // If "https://" and "www" are not present, add them to the URL
+        window.location.href = `https://www.${paymentLink}`;
+      } else {
+        // If "https://" and "www" are already present, use the entered link as is
+        window.location.href = paymentLink;
+      }
+    } else {
+      alert('Please enter a valid payment link.');
+    }
+  };
+
+  const handleInputFocus = () => {
+    setShowWelcomeMessage(false);
+  };
+
+  const handleScrollToTop = () => {
+    window.scrollTo({ top: 0, behavior: 'smooth' });
+  };
+
+  return (
+    <div className="container">
+      <div className="logo-container">
+        <img src="https://images.yourstory.com/cs/wordpress/2018/03/signzy-logo.png?fm=png&auto=format" alt="PaymentHub Logo" className="logo" />
+        <h1>Payment Gateway</h1>
+      </div>
+      {showWelcomeMessage && (
+        <p className="welcome-message">Welcome! Please enter your payment link below to proceed with the payment.</p>
+      )}
+      <div className="input-container">
+        <input
+          type="text"
+          value={paymentLink}
+          onChange={(e) => setPaymentLink(e.target.value)}
+          onFocus={handleInputFocus}
+          placeholder="Paste your payment link here"
+          className="input-field"
+        />
+        <button onClick={redirectToPayment} className="proceed-button" onMouseEnter={handleScrollToTop}>
+          Proceed to Payment
+        </button>
+      </div>
+    </div>
+  );
+}
+
+export default App;
